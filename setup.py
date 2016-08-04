b833f7dc60eacd2c0a62c4173226d1b099467834
goschtl <ck@novareto.de>
2016-08-04 18:18:51 +0200

    Preparing release 0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ test_requires = [
     ]
 
 setup(name='gk.login',
-      version='0.1',
+      version='0.2',
       description="Login system for Gatekeeper",
       long_description = long_description,
       classifiers=['Environment :: Web Environment',
